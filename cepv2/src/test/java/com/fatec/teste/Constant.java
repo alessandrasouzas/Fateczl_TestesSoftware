0c8831b33388a1dc59fcfba146f04dca402004d1
Laboratorio101 <Laboratorio101@LAB101-12>
2019-05-31 15:50:58 -0300

    cep_v2

---
+++ cepv2/src/test/java/com/fatec/teste/Constant.java
@@ -0,0 +1,8 @@
+package com.fatec.teste;
+
+public class Constant {
+	public static final String Path_TestData = "E:\\5 Sem\\Testes\\ed16_\\";
+	public static final String File_TestData = "cep_endereco.xlsx";
+	public static final String Chrome = "WebContent/WEB-INF/lib/chromedriver.exe";
+	public static final String Firefox = "WebContent/WEB-INF/lib/geckodriver.exe";
+}
---
+++ cepv2/src/test/java/com/fatec/teste/UC01ConsultaCEPv2.java
@@ -0,0 +1,14 @@
+package com.fatec.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UC01ConsultaCEPv2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
